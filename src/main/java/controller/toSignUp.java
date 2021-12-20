cae2daa676b96da1637893c8342daef256f47871
tony-200 <a.aschettino5@studenti.unisa.it>
2021-12-20 18:45:59 +0100

    State Pattern

---
+++ src/main/java/controller/toSignUp.java
@@ -0,0 +1,22 @@
+package controller;
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="toSignUp", value="/toSignUp")
+public class toSignUp extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("signUp.jsp");
+        dispatcher.forward(request, response);
+    }
+}
