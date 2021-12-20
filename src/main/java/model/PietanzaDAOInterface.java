4b70699812e5bc308d03bb7ef260b05505f89f75
tony-200 <a.aschettino5@studenti.unisa.it>
2021-12-20 16:30:43 +0100

    interface DAO

--- src/main/java/model/PietanzaDAOInterface.java
+++ src/main/java/model/PietanzaDAOInterface.java
@@ -1,4 +1,4 @@
 package model;
 
-public class PietanzaDAOInterface {
+public interface PietanzaDAOInterface {
 }
--- src/main/java/model/UtenteDAOInterface.java
+++ src/main/java/model/UtenteDAOInterface.java
@@ -1,4 +1,4 @@
 package model;
 
-public class UtenteDAOInterface {
+public interface UtenteDAOInterface {
 }
