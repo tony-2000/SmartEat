3e14ad1208497b9c729273024a38aa0d235018d3
l.vecchione <l.vecchione5@studenti.unisa.it>
2021-12-26 17:02:43 +0100

    AdminStatsArea

--- src/main/java/controller/AdminStatsArea.java
+++ src/main/java/controller/AdminStatsArea.java
@@ -1,6 +1,6 @@
 package controller;
 
-import model.Pietanza;
+import model.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,9 +12,10 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
- *
+ * Classe che implementa la visualizzazione delle statistiche.
  */
 @WebServlet(name="AdminStatsArea", value="/AdminStatsArea")
 public class AdminStatsArea extends HttpServlet
@@ -27,19 +28,50 @@ public class AdminStatsArea extends HttpServlet
     public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         HttpSession session=request.getSession();
-        session.removeAttribute("utenteSessione");
-        RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
-        dispatcher.forward(request, response);
+        Utente u= (Utente) session.getAttribute("utenteSessione");
+        if(u==null)
+            response.sendRedirect(request.getContextPath()+"/index.jsp");
+        assert u != null;
+        RuoloUtente ruoloUtente = u.isAmministratore();
+        if(!ruoloUtente.isAdmin())
+            response.sendRedirect(request.getContextPath()+"/toHome");
+        else
+        {
+            PietanzaDAOInterface pdao = new PietanzaDAO();
+            List<Pietanza> pietanzas= pdao.doRetrieveAllPietanza();
+            List<Integer> statsMenu = this.mostraStatistiche(pietanzas);
+            request.setAttribute("pietanze", pietanzas);
+            request.setAttribute("statsMenu", statsMenu);
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher("AdminStatsArea.jsp");
+            dispatcher.forward(request, response);
+        }
     }
 
-    /**
-     *
-     * @return
+
+    /** Restituisce lista di interi che rappresenta il numero di menu in cui, nella lista di input, la pietanza al numero corrispondente è disponibile.
+     * @param array array di tutte le pietanze
+     * @return lista di interi che rappresenta il numero di menu in cui, nella lista di input, la pietanza al numero corrispondente è disponibile.
      */
-    /*
-    public ArrayList<Pietanza> mostraStatistiche()
-    {
+    public List<Integer> mostraStatistiche(List<Pietanza> array) {
+        MenuDAOInterface mdao = new MenuDAO();
+        List<Menu> menus = mdao.doRetrieveAllmenu();
+        List<Integer> counter = new ArrayList<>();
+        int i = 0;
 
+        for (Pietanza x : array) {
+            int temp = 0;
+            for (Menu y : menus) {
+                if (y.getPrimo().equals(x.getNome()))
+                    temp++;
+                else if (y.getSecondo().equals(x.getNome()))
+                    temp++;
+                else if (y.getDessert().equals(x.getNome()))
+                    temp++;
+            }
+            counter.set(i, temp);
+            i++;
+        }
+        return counter;
     }
-    */
 }
