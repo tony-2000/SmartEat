5cec64835481494480baa5abe89abb62cc5388cf
l.vecchione <l.vecchione5@studenti.unisa.it>
2021-12-29 18:46:05 +0100

    LoginTest (solo metodo login)

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class LoginTest {
@@ -21,26 +21,29 @@ public class LoginTest {
     }
 
     @Test
-    public void loginTestUtenteNonTrovato()
-    {
-        String mail="pippo@mail.it";
-        String password="P4ssword";
-        String[] strings = new String[2];
-        Utente user = new Utente();
-        UtenteDAOInterface udao = Mockito.mock(UtenteDAO.class);
-        login=new Login(udao);
-
-        Mockito.when(udao.doRetrieveUtenteByEmailPassword(mail, password)).thenReturn(user);
-
-        Utente user2 = login.login(mail, password, strings);
-
-        assertEquals(user.getCF(),user2.getCF());
-    }
-
-    @Test
     public void loginTestMailScorretta()
     {
         String mail="pippo";
+        String password="P4ssword!";
+        String[] strings = new String[2];
+        UtenteDAOInterface udao = Mockito.mock(UtenteDAO.class);
+        login=new Login(udao);
+
+        Utente user = new Utente();
+        user.setEmail(mail);
+        user.setPasswordHash(password);
+
+        Mockito.when(udao.doRetrieveUtenteByEmailPassword(mail, password)).thenReturn(user);
+
+        login.login(mail, password, strings);
+
+        assertEquals("Il campo mail deve contenere @", strings[1]);
+    }
+
+    @Test
+    public void loginTestPasswordScorretta()
+    {
+        String mail="pippo@mail.it";
         String password="P4ssword";
         String[] strings = new String[2];
         UtenteDAOInterface udao = Mockito.mock(UtenteDAO.class);
@@ -54,6 +57,29 @@ public class LoginTest {
 
         Utente user2 = login.login(mail, password, strings);
 
-        assertEquals(strings[1], "Il campo mail deve contenere @");
+        assertEquals("Il campo password non ha caratteri speciali", strings[1]);
+    }
+
+    @Test
+    public void loginTestCorretto()
+    {
+        String mail="pippo@mail.it";
+        String password="P4ssword!";
+        String CF="1234567890ABCDEF";
+        String[] strings = new String[2];
+        UtenteDAOInterface udao = Mockito.mock(UtenteDAO.class);
+        login=new Login(udao);
+
+        Utente user = new Utente();
+        user.setEmail(mail);
+        user.setPasswordHash(password);
+        user.setCF(CF);
+
+        Mockito.when(udao.doRetrieveUtenteByEmailPassword(mail, password)).thenReturn(user);
+
+        Utente user2 = login.login(mail, password, strings);
+
+        assertEquals(user, user2);
+        assertNotEquals(null, user2.getCF());
     }
 }
