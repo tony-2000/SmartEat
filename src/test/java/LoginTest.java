7b59d6485d34a4f43c1ee6f1acdfab98cf633acf
l.vecchione <l.vecchione5@studenti.unisa.it>
2021-12-29 19:06:13 +0100

    LoginTest (solo metodo login)

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -33,7 +33,7 @@ public class LoginTest {
         user.setEmail(mail);
         user.setPasswordHash(password);
 
-        Mockito.when(udao.doRetrieveUtenteByEmailPassword(mail, password)).thenReturn(user);
+        Mockito.when(udao.doRetrieveUtenteByEmailPassword(Mockito.anyString(),Mockito.anyString())).thenReturn(user);
 
         login.login(mail, password, strings);
 
@@ -53,11 +53,32 @@ public class LoginTest {
         user.setEmail(mail);
         user.setPasswordHash(password);
 
-        Mockito.when(udao.doRetrieveUtenteByEmailPassword(mail, password)).thenReturn(user);
+        Mockito.when(udao.doRetrieveUtenteByEmailPassword(Mockito.anyString(),Mockito.anyString())).thenReturn(user);
 
-        Utente user2 = login.login(mail, password, strings);
+        login.login(mail, password, strings);
 
         assertEquals("Il campo password non ha caratteri speciali", strings[1]);
+    }
+
+    @Test
+    public void loginTestNotAccepted()
+    {
+        String mail="pippo@mail.it";
+        String password="P4ssword!";
+        String[] strings = new String[2];
+        UtenteDAOInterface udao = Mockito.mock(UtenteDAO.class);
+        login=new Login(udao);
+
+        Utente user = new Utente();
+        user.setEmail(mail);
+        user.setPasswordHash(password);
+        user.setAccepted(false);
+
+        Mockito.when(udao.doRetrieveUtenteByEmailPassword(Mockito.anyString(),Mockito.anyString())).thenReturn(user);
+
+        login.login(mail, password, strings);
+
+        assertEquals("L'amministratore non ha ancora accettato la tua richiesta di registrazione.", strings[0]);
     }
 
     @Test
@@ -75,11 +96,25 @@ public class LoginTest {
         user.setPasswordHash(password);
         user.setCF(CF);
 
-        Mockito.when(udao.doRetrieveUtenteByEmailPassword(mail, password)).thenReturn(user);
+        Mockito.when(udao.doRetrieveUtenteByEmailPassword(Mockito.anyString(),Mockito.anyString())).thenReturn(user);
 
         Utente user2 = login.login(mail, password, strings);
 
         assertEquals(user, user2);
         assertNotEquals(null, user2.getCF());
+    }
+
+    @Test
+    public void doPostTestCorretto()
+    {
+        Utente user = new Utente();
+        user.setEmail("pippo@mail.it");
+        user.setPasswordHash("P4ssword!");
+        user.setCF("1234567890ABCDEF");
+        UtenteDAOInterface udao = Mockito.mock(UtenteDAO.class);
+        login=new Login(udao);
+
+        Mockito.when(udao.doRetrieveUtenteByEmailPassword(Mockito.anyString(),Mockito.anyString())).thenReturn(user);
+
     }
 }
