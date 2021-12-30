efae8ee6f83afeeab69276d043e6eebbe99a244c
tony-200 <a.aschettino5@studenti.unisa.it>
2021-12-30 17:01:41 +0100

    ocl Esito

--- src/main/java/model/Esito.java
+++ src/main/java/model/Esito.java
@@ -1,25 +1,54 @@
 package model;
 
-public class Esito {
-
-    private String message;
+/**
+ * Questa classe di servizio realizza la convalida degli input tramite un valore di validita' ed
+ * un eventuale messaggio di errore.
+ */
+public class Esito
+{
+    /**
+     * Validita' dell'input
+     */
     private boolean valido;
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    /**
+     * Stringa di errore, nel caso ci sia.
+     */
+    private String message;
 
 
 
+    /** Verifica se l'input e' valido.
+     * @return
+     * Restituisce il valore che verifica se l'input e' valido.
+     */
     public boolean isValido() {
         return valido;
     }
 
+    /**
+     * Imposta se l'input e' valido.
+     * @param valido validita' dell'input.
+     */
     public void setValido(boolean valido) {
         this.valido = valido;
+    }
+
+
+
+    /** Restituisce l'eventuale messaggio di errore.
+     * @return
+     * Restituisce l'eventuale messaggio di errore.
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Imposta l'eventuale stringa di errore
+     * @param message eventuale stringa di errore.
+     */
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
