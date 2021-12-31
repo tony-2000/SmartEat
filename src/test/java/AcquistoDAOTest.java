03f5604646757a90fc1ae229a16f0173d840e690
tony-200 <a.aschettino5@studenti.unisa.it>
2021-12-31 17:08:40 +0100

    AcquistoDAOTest.java

--- src/test/java/AcquistoDAOTest.java
+++ src/test/java/AcquistoDAOTest.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.sql.Date;
+import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class AcquistoDAOTest extends Mockito
 {
@@ -31,6 +31,82 @@ public class AcquistoDAOTest extends Mockito
         assertEquals(codiceMenu,acquisto.getCodiceMenu());
         assertEquals(dataAcquisto,acquisto.getDataAcquisto());
         assertTrue(acquisto.isPostoMensa());
+    }
+
+
+    @Test
+    public void doRetrieveAllAcquistoByCFTest()
+    {
+        String CF="RSSMRA74D22A0011";
+        ArrayList<Acquisto> acquisto= (ArrayList<Acquisto>) acquistoDAO.doRetrieveAllAcquistoByCF(CF);
+        int i=0;
+        for(Acquisto x: acquisto)
+        {
+            assertEquals(CF,acquisto.get(i).getCF());
+            i++;
+        }
+    }
+
+    @Test
+    public void doSaveTest()
+    {
+        Date dataAcquisto=Date.valueOf("2021-12-10");
+        String CF="RSSMRA74D22A0011";
+        int codiceMenu=2;
+        boolean pm=true;
+        Acquisto acquisto=new Acquisto();
+        acquisto.setDataAcquisto(dataAcquisto);
+        acquisto.setPostoMensa(pm);
+        acquisto.setCodiceMenu(codiceMenu);
+        acquisto.setCF(CF);
+        acquistoDAO.doSave(acquisto);
+
+        Acquisto acquisto2;
+        acquisto2=acquistoDAO.doRetrieveAcquistoByKey(dataAcquisto,CF,codiceMenu);
+
+        assertEquals(acquisto2.getCF(),acquisto.getCF());
+        assertEquals(acquisto2.getCodiceMenu(),acquisto.getCodiceMenu());
+        assertEquals(acquisto2.getDataAcquisto(),acquisto.getDataAcquisto());
+        assertEquals(acquisto2.isPostoMensa(),acquisto.isPostoMensa());
+    }
+
+
+    @Test
+    public void doUpdateTest()
+    {
+        Acquisto acquisto= acquistoDAO.doRetrieveAcquistoByKey(Date.valueOf("2021-12-10"),"RSSMRA74D22A0011",2);
+        boolean postoMensa=acquisto.isPostoMensa();
+
+        acquisto.setPostoMensa(false);
+        acquistoDAO.doUpdate(acquisto);
+
+        Acquisto acquisto2;
+        acquisto2=acquistoDAO.doRetrieveAcquistoByKey(Date.valueOf("2021-12-10"),"RSSMRA74D22A0011",2);
+
+        assertEquals(acquisto2.getCF(),acquisto.getCF());
+        assertEquals(acquisto2.getCodiceMenu(),acquisto.getCodiceMenu());
+        assertEquals(acquisto2.getDataAcquisto(),acquisto.getDataAcquisto());
+        assertNotEquals(acquisto2.isPostoMensa(),postoMensa);
+    }
+
+
+
+    @Test
+    public void doDelete()
+    {
+        Date dataAcquisto=Date.valueOf("2021-12-10");
+        String CF="RSSMRA74D22A0011";
+        int codiceMenu=2;
+        acquistoDAO.doDelete(dataAcquisto,CF,codiceMenu);
+
+        Acquisto acquisto2;
+        acquisto2=acquistoDAO.doRetrieveAcquistoByKey(dataAcquisto,CF,codiceMenu);
+
+        assertNull(acquisto2.getCF());
+        assertNull(acquisto2.getDataAcquisto());
+        assertEquals(acquisto2.getCodiceMenu(),0);
+        assertFalse(acquisto2.isPostoMensa());
+
     }
 
 }
