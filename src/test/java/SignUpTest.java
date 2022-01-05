8805d5a3ec8457ec109ccfecdf1881b0deb80c51
tony-200 <a.aschettino5@studenti.unisa.it>
2022-01-05 19:44:49 +0100

    Check.java

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -4,12 +4,20 @@ import model.UtenteDAOInterface;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.sql.Date;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
 
 public class SignUpTest
 {
@@ -231,4 +239,51 @@ public class SignUpTest
     }
 
 
+    @Test
+    public void doPostTestErrore() throws ServletException, IOException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getSession()).thenReturn(session);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+
+        when(request.getParameter("CF")).thenReturn("AAAAAAAAAAAAAAAA");
+        when(request.getParameter("nome_utente")).thenReturn("antonio");
+        when(request.getParameter("cognome")).thenReturn("Aschettino");
+        when(request.getParameter("gender")).thenReturn("M");
+        when(request.getParameter("dataDiNacita")).thenReturn("2000-06-24");
+        when(request.getParameter("luogoDiNascita")).thenReturn("Nola");
+        when(request.getParameter("mail")).thenReturn("antonio@gmail.com");
+        when(request.getParameter("residenza")).thenReturn("Lauro");
+        when(request.getParameter("password")).thenReturn("P4ssword!");
+        when(request.getParameter("passwordCheck")).thenReturn("P4ssword!");
+
+        when(signup.registrazione(anyString(),anyString(),anyString(),anyString(),any(Date.class),anyString(),anyString(),anyString(),anyString(),anyString())).thenReturn("errore");
+
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+        verify(dispatcher, atLeastOnce()).forward(request, response);
+    }
+
+    @Test
+    public void doPostTestCorretto() throws ServletException, IOException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getSession()).thenReturn(session);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+
+        when(request.getParameter("CF")).thenReturn("AAAAAAAAAAAAAAAA");
+        when(request.getParameter("nome_utente")).thenReturn("antonio");
+        when(request.getParameter("cognome")).thenReturn("Aschettino");
+        when(request.getParameter("gender")).thenReturn("M");
+        when(request.getParameter("dataDiNacita")).thenReturn("2000-06-24");
+        when(request.getParameter("luogoDiNascita")).thenReturn("Nola");
+        when(request.getParameter("mail")).thenReturn("antonio@gmail.com");
+        when(request.getParameter("residenza")).thenReturn("Lauro");
+        when(request.getParameter("password")).thenReturn("P4ssword!");
+        when(request.getParameter("passwordCheck")).thenReturn("P4ssword!");
+
+        when(signup.registrazione(anyString(), anyString(), anyString(), anyString(), any(Date.class), anyString(), anyString(), anyString(), anyString(), anyString())).thenReturn("");
+
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+        verify(dispatcher, atLeastOnce()).forward(request, response);
+    }
 }
